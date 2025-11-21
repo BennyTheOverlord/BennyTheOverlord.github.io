fe5368307aed40b2f6d91e5496694d73de042bbf
BennyTheOverlord <benlodtzhauge@gmail.com>
2025-11-21 10:10:00 +0100

    Test publisering

--- Prosjektarbeid/Quiz.js
+++ Prosjektarbeid/Quiz.js
@@ -21,11 +21,30 @@ let spørsmålContainer = [
         ],
         fasit: 2
     },
+
+    {
+        spørsmål: "Hvor mange timer er det i en uke?",
+        tema: "Tid",
+        svarAlternativer: [
+            "168",
+            "172",
+            "156",
+            "178"
+        ],
+        fasit: 0
+    }
 ];
 
-
+let svarKnapper = document.getElementById("svar-Container");
 let aktivSpørsmål = document.getElementById("spørsmål-Container"); //Brukes til å vise det aktive spørsmålet, som er satt i "spørsmål-Container" i html filen
 let aktivSpørsmålIndex = 0; //Brukes til å finne det riktige spørsmålet i arrayen, slik at man kan sjekke svaret
+
+let answer1 = document.getElementById("svar1");
+let answer2 = document.getElementById("svar2");
+let answer3 = document.getElementById("svar3");
+let answer4 = document.getElementById("svar4");
+
+
 
 function definerSvar(spørsmål) {
     for (let i = 0; i < spørsmålContainer[spørsmål].svarAlternativer.length; i++) {
@@ -34,24 +53,34 @@ function definerSvar(spørsmål) {
     aktivSpørsmål.innerHTML = spørsmålContainer[spørsmål].spørsmål;
 };
 
-definerSvar(0);
+definerSvar(aktivSpørsmålIndex);
 
 function sjekkSvar(valgtSvar) {
     let riktigSvar = spørsmålContainer[aktivSpørsmålIndex].fasit;
 
+    let valgtAlternativ = document.getElementById("svar" + (valgtSvar + 1));
+
     if (valgtSvar === riktigSvar) {
-        console.log("Du svarte riktig")
+        console.log("Du svarte riktig");
+
+        aktivSpørsmålIndex++;
+        definerSvar(aktivSpørsmålIndex);
         
      } else {
         console.log("Du svarte feil")
+        valgtAlternativ.style.backgroundColor = "red";
+
+        setTimeout(function() {
+            valgtAlternativ.style.backgroundColor = "white"; //Sett in "white" igjen senere
+
+             aktivSpørsmålIndex++;
+            definerSvar(aktivSpørsmålIndex);
+        }, 1000);
+
      }
 
     };
 
-let answer1 = document.getElementById("svar1");
-let answer2 = document.getElementById("svar2");
-let answer3 = document.getElementById("svar3");
-let answer4 = document.getElementById("svar4");
 
 answer1.addEventListener("click", function() {
     sjekkSvar(0);
